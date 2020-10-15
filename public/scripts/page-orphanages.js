d080434dbbb381892e6fb519d64b1ddced813d07
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-15 15:39:09 -0300

    Happy: Page-Orphanages.js[+]
    Criado funções para criar icone e popup ao clicar na marcação.

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,9 +1,25 @@
 // Código para criação do mapa - Usando biblioteca Leaflet
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-21.2034112,-41.8977974], 15);
 
 // Adicionando o mapa na const criada
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Criando icone para ser add no map ao criar uma marcação
+const icon =L.icon({
+    iconUrl: "./public/assets/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+// Criando popup para abrir ao clicar na marcação
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanages"> <img src="./public/assets/arrow-white.svg"> </a>');
+
+// Adicionado marcação para inserir popups
+L.marker([-21.2034112,-41.8977974], {icon}).addTo(map)
+    .bindPopup(popup);
