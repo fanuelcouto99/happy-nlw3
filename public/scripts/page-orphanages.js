1f10b9766fd52222063564ec3bd06317fff26d1a
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-15 15:00:01 -0300

    Happy: Page-Orphanages.js[+]
    Adicionado configuração para visualização e criação do mapa.

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,2 +1,9 @@
 // Código para criação do mapa - Usando biblioteca Leaflet
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Adicionando o mapa na const criada
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+).addTo(map);
