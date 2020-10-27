c01d7807967cb9bfa3a713df400dde0ac5ae413c
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-27 13:38:22 -0300

    Happy: Node Modules
    Instalado package hbs para template engine do projeto

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
+// Configurando caminho dos arquivos para evitar erros ao rodar no windows
+const path = require('path');
+
 // Configurando servidor
 const express = require('express');
 const server = express();
 
-// Configurando as rotas da aplicação
-server.get('/', () => {
-    
-})
+// configurando arquivos estaticos (css, js)
+server.use(express.static('public'))
+
+    // Configurando as rotas da aplicação
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'));
+    })
 
 server.listen(5500);
