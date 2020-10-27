283a1f91e21ff2ccadcf2891a1d59d84c4fc081e
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-27 10:48:29 -0300

    Happy: Node Module[+]
    Instalado package nodemon para monitoramento do servidor.

---
+++ src/server.js
@@ -0,0 +1,10 @@
+// Configurando servidor
+const express = require('express');
+const server = express();
+
+// Configurando as rotas da aplicação
+server.get('/', () => {
+    
+})
+
+server.listen(5500);
