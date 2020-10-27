b9ca6c169a6f0ed7ee07334f5bcfcc71d1dd9851
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-27 15:13:53 -0300

    Happy; Page Orphanages.js[*]
    Criado função para criar marcação no mapa para cada orfanato criado.

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,25 +1,40 @@
 // Código para criação do mapa - Usando biblioteca Leaflet
-const map = L.map('mapid').setView([-21.2034112,-41.8977974], 15);
+const map = L.map('mapid').setView([-21.2034112, -41.8977974], 15);
 
 // Adicionando o mapa na const criada
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // Criando icone para ser add no map ao criar uma marcação
-const icon =L.icon({
+const icon = L.icon({
     iconUrl: "/assets/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 });
 
-// Criando popup para abrir ao clicar na marcação
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanages"> <img src="/assets/arrow-white.svg"> </a>');
+function addMarker({ id, name, lat, lng }) {
+    // Criando popup para abrir ao clicar na marcação
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/assets/arrow-white.svg"> </a>`);
 
-// Adicionado marcação para inserir popups
-L.marker([-21.2034112,-41.8977974], {icon}).addTo(map)
-    .bindPopup(popup);
+    // Adicionado marcação para inserir popups
+    L.marker([lat, lng], { icon }).addTo(map)
+        .bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+
+orphanagesSpan.forEach(((orphanageElement) => {
+    const orphanage = {
+        id: orphanageElement.dataset.id,
+        name: orphanageElement.dataset.name,
+        lat: orphanageElement.dataset.lat,
+        lng: orphanageElement.dataset.lng
+    }
+
+    addMarker(orphanage);
+})) 
