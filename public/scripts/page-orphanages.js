8bf3689bd9de12826e1d19ad6e479f8960a26562
fanuelcouto99 <fanuel.couto99@gmail.com>
2020-10-27 14:17:51 -0300

    Happy: *.js[*]
    Alterado importados dos arquivos estaticos e corrigido href das paginas

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // Criando icone para ser add no map ao criar uma marcação
 const icon =L.icon({
-    iconUrl: "./public/assets/map-marker.svg",
+    iconUrl: "/assets/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
@@ -18,7 +18,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanages"> <img src="./public/assets/arrow-white.svg"> </a>');
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanages"> <img src="/assets/arrow-white.svg"> </a>');
 
 // Adicionado marcação para inserir popups
 L.marker([-21.2034112,-41.8977974], {icon}).addTo(map)
